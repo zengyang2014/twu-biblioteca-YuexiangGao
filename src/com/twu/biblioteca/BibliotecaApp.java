ec638da8585bda385b77f4ac3f101121cf56a242
gaoyx <melo@gaoyuexiang.cn>
2017-07-27 22:53:39 +0800

    [close #1] print welcome message in main method

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,7 +3,8 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+        System.out.println(app.getWelcome());
     }
 
     public String getWelcome() {
