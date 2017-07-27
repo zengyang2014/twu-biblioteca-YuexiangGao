a2aafc93e69b325e8feb5eeeda63afc4cd3494ae
gaoyx <melo@gaoyuexiang.cn>
2017-07-27 22:55:16 +0800

    [open #2] add model Book

---
+++ src/com/twu/biblioteca/model/Book.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca.model;
+
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return getName() != null ? getName().equals(book.getName()) : book.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getName() != null ? getName().hashCode() : 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
