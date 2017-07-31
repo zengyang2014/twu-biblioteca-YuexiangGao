d343a3bcb928a4de545bda3c076e3ef555d15172
gaoyx <melo@gaoyuexiang.cn>
2017-07-31 16:26:21 +0800

    [#13] add model Movie

---
+++ src/com/twu/biblioteca/model/Movie.java
@@ -0,0 +1,70 @@
+package com.twu.biblioteca.model;
+
+public class Movie {
+
+    private String name;
+    private int year;
+    private String director;
+    private int rating;
+
+    public Movie(String name, int year, String director, int rating) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (getYear() != movie.getYear()) return false;
+        if (getRating() != movie.getRating()) return false;
+        if (getName() != null ? !getName().equals(movie.getName()) : movie.getName() != null) return false;
+        return getDirector() != null ? getDirector().equals(movie.getDirector()) : movie.getDirector() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + getYear();
+        result = 31 * result + (getDirector() != null ? getDirector().hashCode() : 0);
+        result = 31 * result + getRating();
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
